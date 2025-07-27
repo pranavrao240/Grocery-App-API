e89f3043f17543b2eff710e80e093a72d4433ca2
PRANAV HIRAMAN RAO <93645267+pranavrao240@users.noreply.github.com>
2025-07-27 16:45:40 +0530

    Create app.config.js
    Signed-off-by: PRANAV HIRAMAN RAO <93645267+pranavrao240@users.noreply.github.com>

---
+++ config/app.config.js
@@ -0,0 +1,14 @@
+const  MONGO_DB_CONFIG = {
+    DB:"mongodb://localhost/myDB"
+    ,
+    PAGE_SIZE:10
+
+};
+const STRIPE_CONFIG = {
+    STRIPE_KEY :"sk_test_tR3PYbcVNZZ796tH88S4VQ2u",
+    CURRENCY :'inr'
+}
+module.exports={
+    MONGO_DB_CONFIG,
+    STRIPE_CONFIG
+}
